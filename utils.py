0c47cf720e0aef31856c7377a7e2bf752da1d4f0
Joaquin-Parrilla <jparrilla@scanntech.com>
2022-01-08 15:20:34 -0300

    add generate_key and get_key

--- utils.py
+++ utils.py
@@ -1,16 +1,33 @@
 import cryptography
 from cryptography.fernet import Fernet
 
+KEY_PATH = "data/key.key"
 
 def get_file(path):
     pass
 
 
-def encrypt():
+def generate_key() -> bytes:
+    key = Fernet.generate_key()
+    with open(KEY_PATH, "wb") as file:
+        file.write(key)
+    return key
+
+
+def get_key():
+    with open(KEY_PATH) as file:
+        key = file.read()
+    
+    if key is None: # or len(key) == 0:
+        return generate_key()
+    return key
+
+
+def encrypt(path: str):
     pass
 
 
-def decrypt():
+def decrypt(path: str):
     pass
 
 
