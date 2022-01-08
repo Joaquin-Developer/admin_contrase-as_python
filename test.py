17ee2aa8dcad6015c8e094051ad91a0a9fe7ccc1
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-08 19:53:55 -0300

    agrego modulos de test de funciones

--- test.py
+++ test.py
@@ -1,9 +1,41 @@
 import utils
+import json
 
 
 def main():
-    pass
+    # datos de prueba:
+    data = [
+        {
+            "index": 0,
+            "site": "instagram",
+            "username": "Joaquin12345",
+            "password": "12345"
+        },
+        {
+            "index": 1,
+            "site": "correo gmail",
+            "username": "usuario@gmail.com",
+            "password": "67890"
+        }
+    ]
+    # texto a json:
+    json_data = json.dumps(data)
+    print(json_data)
+    print(type(json_data))
+    # reconvertir: json a texto:
+    data_original = json.loads(json_data)
+    print(data_original)
+    print(type(data_original))
 
+
+def load():
+    with open("data/test.json") as file:
+        data = file.read()
+
+    print(data)
+    json_data = json.loads(data)
+    print(json_data)
+    print(type(json_data))
 
 if __name__ == "__main__":
-    main()
+    load()
