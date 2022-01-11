d7d2a3c96d4de6fe320a4d639dcb4734db27ac56
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-11 15:28:13 -0300

    update config.py

--- core/config.py
+++ core/config.py
@@ -2,7 +2,8 @@ APP_NAME = "Administrador de Contraseñas"
 
 gui = {
     "size": {
-        "window": "650x500"
+        "window": "500x250"
     },
-    "lbl_title": "Lista de contraseñas guardadas"
+    "lbl_title": "Lista de contraseñas guardadas:",
+    "exit_button_text": "Salir"
 }
